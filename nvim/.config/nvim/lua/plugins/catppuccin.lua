b6c15019fdaf2566989dd68e00e37eaa73a3a3b1
Zach Brown <zach@zb.dev>
2025-01-11 13:35:30 -0800

    enable more catppuccin plugins

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,12 +4,23 @@ return {
     lazy = false,
     name = "catppuccin",
     priority = 1000,
-
     config = function()
       require("catppuccin").setup({
-        transparent_background = true,
+        --        transparent_background = true,
+        integrations = {
+          noice = true,
+          notify = true,
+          alpha = true,
+          bufferline = true,
+          fzf = true,
+          mason = true,
+          neotree = true,
+          telescope = {
+            enabled = true,
+          }
+        }
       })
-      vim.cmd.colorscheme "catppuccin-mocha"
+      vim.cmd.colorscheme "catppuccin-macchiato"
     end
   }
 }
